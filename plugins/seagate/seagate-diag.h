c4e2e3b9a0390caaf039e2c61f3d23ab62a1f8c2
Muhammad Ahmad <ahmadjee@users.noreply.github.com>
2018-12-21 10:57:00 -0600

    Fixing formatting

--- plugins/seagate/seagate-diag.h
+++ plugins/seagate/seagate-diag.h
@@ -3,7 +3,7 @@
 //
 // Copyright (c) 2017-2018 Seagate Technology LLC and/or its Affiliates
 //
-// ******************************************************************************************
+// ********************************************************************
 // 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
